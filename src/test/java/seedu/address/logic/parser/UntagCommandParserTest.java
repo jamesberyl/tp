24f39cbdfa01b95bdd7ee880aa151d4d218a3c98
Beryl <a.j.beryl@gmail.com>
2025-04-05 23:17:49 +0800

    Update test

--- src/test/java/seedu/address/logic/parser/UntagCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/UntagCommandParserTest.java
@@ -1,14 +1,18 @@
 package seedu.address.logic.parser;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.TagCommand;
 import seedu.address.logic.commands.UntagCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.wedding.WeddingId;
 
 public class UntagCommandParserTest {
@@ -27,20 +31,23 @@ public class UntagCommandParserTest {
         assertParseFailure(parser, "",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, UntagCommand.MESSAGE_USAGE));
 
-        // Only index
+        // Only index provided
         assertParseFailure(parser, "1",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, UntagCommand.MESSAGE_USAGE));
 
-        // Invalid index
-        assertParseFailure(parser, "a W123",
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, UntagCommand.MESSAGE_USAGE));
+        // Non-numeric index provided
+        assertParseFailure(parser, "a W123", MESSAGE_INVALID_INDEX);
     }
 
     @Test
-    public void parse_invalidWeddingId_throwsIllegalArgumentException() {
-        // These cases will cause IllegalArgumentException due to WeddingId validation
-        assertThrows(IllegalArgumentException.class, () -> parser.parse("1 123"));
-        assertThrows(IllegalArgumentException.class, () -> parser.parse("1 W"));
+    public void parse_invalidWeddingId_throwsParseException() {
+        // Wedding ID "123" is invalid because it doesn't start with 'W'
+        ParseException pe1 = assertThrows(ParseException.class, () -> parser.parse("1 123"));
+        assertEquals(WeddingId.MESSAGE_CONSTRAINTS, pe1.getMessage());
+
+        // Wedding ID "W" is invalid because it doesn't have any digits following 'W'
+        ParseException pe2 = assertThrows(ParseException.class, () -> parser.parse("1 W"));
+        assertEquals(WeddingId.MESSAGE_CONSTRAINTS, pe2.getMessage());
     }
 
     @Test
